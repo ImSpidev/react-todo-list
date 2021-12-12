874d4566a121f9d9bf8d4716685ce6bae3082ada
JosÃe Carlos Arreola <charly_cto@hotmail.com>
2021-12-11 22:28:24 -0600

    Reorganización de ccarpetas y refactorización de código

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
-
-
+import { AppUI } from './AppUI';
 import './App.css';
 
 const defaultTodos = [
@@ -54,33 +48,18 @@ function App() {
   }
 
   return (
-    <React.Fragment>
-      <div className="MainContainer">
-          <TodoCounter 
-            total={totalTodos}
-            completed={completedTodos}
-          />
-          <TodoSearch 
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-          />
-            
-          <TodoList>
-            {searchedTodos.map((todo) => 
-              <TodoItem 
-              key={todo.text} 
-              text={todo.text} 
-              completed={todo.completed}
-              onComplete={() => completeTodo(todo.text)}
-              onDelete={() => deleteTodo(todo.text)}
-              />
-            )}  
-          </TodoList>
-          
-          
-          <CreateTodoButton />
-      </div>
-    </React.Fragment>
+    <AppUI 
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+
+      searchedTodos={searchedTodos}
+
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
